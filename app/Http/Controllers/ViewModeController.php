1edca52ad1d47704bca5a441a1b77d8820a5238d
Muhamad Azizi <muhamadazizi188@gmail.com>
2023-02-26 14:41:14 +0700

    Update tampilan pada view mode
    Update view
    
    change logic in penggunaan to perhari
    
    tambah logo

--- app/Http/Controllers/ViewModeController.php
+++ app/Http/Controllers/ViewModeController.php
@@ -13,6 +13,10 @@ class ViewModeController extends Controller
     {
         $data = [
             'title' => 'Jadwal',
+            'penggunaan' => Penggunaan::where('status', 'Diterima')
+                ->where('tanggal_penggunaan', date('Y-m-d'))
+                ->orderBy('jam_masuk', 'asc')
+                ->get()
         ];
 
         // Pembagian sesi waktu jadwal
@@ -37,12 +41,6 @@ class ViewModeController extends Controller
                     ->where('jadwal.jam_mulai', '<=', $sesi_pertama['jam_keluar'])
                     ->orderBy('jadwal.jam_mulai', 'asc');
             })->where('verifikasi_jadwal.created_at', 'like', date('Y-m-d') . '%')->get();
-            $data['penggunaan'] = Penggunaan::where('status', 'Diterima')
-                ->where('tanggal_penggunaan', date('Y-m-d'))
-                ->where('jam_masuk', '>=', $sesi_pertama['jam_masuk'])
-                ->where('jam_masuk', '<=', $sesi_pertama['jam_keluar'])
-                ->orderBy('jam_masuk', 'asc')
-                ->get();
         } elseif ($sekarang >= $sesi_kedua['jam_masuk'] && $sekarang <= $sesi_kedua['jam_keluar']) {
             $data['sesi'] = 'Sesi 2' . ' (' . $sesi_kedua['jam_masuk'] . ' - ' . $sesi_kedua['jam_keluar'] . ')';
             $data['verifikasi_jadwal'] = VerifikasiJadwal::whereHas('jadwal', function ($query) use ($sesi_kedua) {
@@ -50,12 +48,6 @@ class ViewModeController extends Controller
                     ->where('jadwal.jam_mulai', '<=', $sesi_kedua['jam_keluar'])
                     ->orderBy('jadwal.jam_mulai', 'asc');
             })->where('verifikasi_jadwal.created_at', 'like', date('Y-m-d') . '%')->get();
-            $data['penggunaan'] = Penggunaan::where('status', 'Diterima')
-                ->where('tanggal_penggunaan', date('Y-m-d'))
-                ->where('jam_masuk', '>=', $sesi_kedua['jam_masuk'])
-                ->where('jam_masuk', '<=', $sesi_kedua['jam_keluar'])
-                ->orderBy('jam_masuk', 'asc')
-                ->get();
         } elseif ($sekarang >= $sesi_ketiga['jam_masuk'] && $sekarang <= $sesi_ketiga['jam_keluar']) {
             $data['sesi'] = 'Sesi 3' . ' (' . $sesi_ketiga['jam_masuk'] . ' - ' . $sesi_ketiga['jam_keluar'] . ')';
             $data['verifikasi_jadwal'] = VerifikasiJadwal::whereHas('jadwal', function ($query) use ($sesi_ketiga) {
@@ -63,12 +55,6 @@ class ViewModeController extends Controller
                     ->where('jadwal.jam_mulai', '<=', $sesi_ketiga['jam_keluar'])
                     ->orderBy('jadwal.jam_mulai', 'asc');
             })->where('verifikasi_jadwal.created_at', 'like', date('Y-m-d') . '%')->get();
-            $data['penggunaan'] = Penggunaan::where('status', 'Diterima')
-                ->where('tanggal_penggunaan', date('Y-m-d'))
-                ->where('jam_masuk', '>=', $sesi_ketiga['jam_masuk'])
-                ->where('jam_masuk', '<=', $sesi_ketiga['jam_keluar'])
-                ->orderBy('jam_masuk', 'asc')
-                ->get();
         } else {
             $data['sesi'] = '';
             $data['verifikasi_jadwal'] = [];
